f9b53df2c30050f02697f21ef86c589fe0109992
Gowsalya Sanjai <145602948+gowsiraj@users.noreply.github.com>
2024-01-23 23:10:02 +0400

    Update Card.jsx

--- src/Shop/Card.jsx
+++ src/Shop/Card.jsx
@@ -7,7 +7,7 @@ function Card({ data, clicked }) {
         <img src={data.picture} class="card-img-top img-1" alt="..." />
         <div class="card-body">
           <h5 class="card-title">{data.head}</h5>
-          <p class="card-text">{data.price} / 30 piecs</p>
+          <p class="card-text">RS: {data.price} / 30 piecs</p>
           {/* <button onClick={addCart;{()=>clicked(data.id)}} class="btn btn-success">{data.isclicked ? "Added" : "Add"} to Cart</button> */}
           <button onClick={() => { clicked(data.id); }} className="btn btn-success">
             {data.isclicked ? "Remove from" : "Add to"}  Cart
